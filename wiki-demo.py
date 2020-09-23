f0def4b50b37a0c2f0080fdc63b3df58afcba3be
lyh01 <huy_ly@hotmail.com>
2020-09-23 09:13:55 -0500

    Update wiki-demo.py

--- wiki-demo.py
+++ wiki-demo.py
@@ -6,25 +6,22 @@ import requests
 # Process --page and --question from command line
 #
 
-parser = argparse.ArgumentParser(description = "This demo will retrieve a Wikipedia page (--page), and answer a question (--question) based on its content")
-parser.add_argument('-p', '--page', help="input the wiki page, e.g., \"chicago bears\"", type=str, required=True )
+parser = argparse.ArgumentParser(description = "This demo will retrieve a Wikipedia page (--page), and answer a question (--question) based on the page content")
+parser.add_argument('-p', '--page', help="input the wiki page, e.g., \"chicago_bears\"", type=str, required=True )
 parser.add_argument('-q', '--question', help="ask a question, e.g., \"when did the bears move?\"", type=str, required = True)
 args = parser.parse_args()
 
-#
-# wiki page content in result.content
-#
-resultWiki = wiki.WikipediaPage(title=args.page)
-
-#
-#  set up for API call
-#
+try:
+    resultWiki = wiki.WikipediaPage(title=args.page)
+except:
+    print("\nAn error occurred while retrieving from Wikipedia. Please ensure the wiki page does exist\n")
+    exit(12)
 
 headers = {
     'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': 'your-api-key-goes-here',
-
+    'Ocp-Apim-Subscription-Key': '130743a0539e4c82962b1c2567a57e54',
 }
+
 params = {
 
 }
@@ -47,4 +44,5 @@ except Exception as e:
 
 print("\nBest answer for \n\nQ: \"{}\"\nA: {}\n   (score: {:.2f})\n\nusing this passage:\n\n\"{}\"\n\n".
       format(respJSON['question'], respJSON['answers'][0]['Answer'], respJSON['answers'][0]['Score'],
-      respJSON['answers'][0]['Passage']))
+      respJSON['answers'][0]['Passage'], 
+      ))
